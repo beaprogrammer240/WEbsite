15505ac7bd133941ca8f66c6a7b56a3021f51b69
beaprogrammer240 <86970674+beaprogrammer240@users.noreply.github.com>
2021-07-06 08:10:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // Perform your AJAX/Fetch login
 
-        setFormMessage(loginForm, "error", "Invalid username/password combination");
+        setFormMessage(loginForm, "error", "Succesfully Logged In ");
     });
 
     document.querySelectorAll(".form__input").forEach(inputElement => {
